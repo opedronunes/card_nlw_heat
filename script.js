c4aed30976dce3e7d41700f4174ed4d13cab6d66
Pedro Nunes <81447137+PedroNunes-Dev@users.noreply.github.com>
2021-10-23 23:55:59 -0300

    Novas funcionalidades com JS

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const linksSocialMedia = {
-  github: 'Pedronunes - Dev',
-  youtube: 'chanel/',
-  instagram: 'pedruhnunes',
-  facebook: 'maykbrito',
-  twitter: 'janelinytec'
+    github: 'PedroNunes-Dev',
+    youtube: 'channel/UCe9iYY_tT-Riu3nCFAfsqAQ',
+    facebook: 'maykbrito',
+    instagram: 'pedruhnunes',
+    twitter: 'jakelinytec'
 }
 
 function changeSocialMediaLinks() {
-  for (let li of socialLinks.children) {
-    const social = li.getAttribute('class')
-    li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}`
-  }
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
 }
 changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userPhoto.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
